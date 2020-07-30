0df806feddc029bfe2b4ddc54bc59a27925c39cc
Joon Jang <w.joon.jang@gmail.com>
2020-07-29 23:34:15 -0700

    - can go back and see previous weight

--- FitWatch/FitWatch/ViewModel/WorkoutViewModel.cs
+++ FitWatch/FitWatch/ViewModel/WorkoutViewModel.cs
@@ -22,11 +22,16 @@ namespace FitWatch.ViewModel
 
         public Command NextCommand { get; }
         public Command PreviousCommand { get; }
+        public Command<string> AddCommand { get; }
+        public Command<string> SubtractCommand { get; }
 
         public WorkoutViewModel()
         {
             NextCommand = new Command(On_NextWorkoutInfo, (x) => CanGoNext);
             PreviousCommand = new Command(On_PreviousWorkoutInfo, (x) => CanGoBack);
+
+            AddCommand = new Command<string>(IncreaseWeight);
+            SubtractCommand = new Command<string>(SubtractWeight);
 
             MessagingCenter.Subscribe<object>(Application.Current, "Parse", (s) =>
             {
@@ -39,37 +44,7 @@ namespace FitWatch.ViewModel
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        private void On_NextWorkoutInfo(object obj)
-        {
-            NextWorkoutInfo();
-        }
-
-        private bool canGoNext = true;
-        public bool CanGoNext
-        {
-            get=>canGoNext;
-            set
-            {
-                canGoNext = value;
-                ((Command)NextCommand).ChangeCanExecute();
-            }
-        }
-
-        private void On_PreviousWorkoutInfo(object obj)
-        {
-            PreviousWorkoutInfo();
-        }
-
-        private bool canGoBack = false;
-        public bool CanGoBack
-        {
-            get => canGoBack;
-            set
-            {
-                canGoBack = value;
-                ((Command)PreviousCommand).ChangeCanExecute();
-            }
-        }
+        // json logic -----------------------
 
         public void ParseJson()
         {
@@ -112,23 +87,174 @@ namespace FitWatch.ViewModel
 
             }
 
+            // start from the beggining workout after parse
+
             WorkoutTitletString = workouts[0];
             RepString = reps[0];
             WeightString = weights[0];
 
+            // add the starting data array week and day information
+            // first data array 
+            NewWeightList.Add(jsonObject.Week);
+            NewWeightList.Add(jsonObject.Day);
+            DataArrayList.Add(NewWeightList);
+
+            // clear for next array 
+            NewWeightList = new List<string>();
+
         }
+
+        // logic for previous and next workout -------------------
 
         int workAndRepInt = 0;
         int weightInt = 0;
 
+        private int newWeightInt;
+        public int NewWeightInt
+        {
+            get => newWeightInt;
+            set
+            {
+                newWeightInt = value;
+                OnPropertyChanged();
+            }
+        }
+
+        
+
+        void IncreaseWeight(string i)
+        {
+            if (int.TryParse(i, out int num))
+            {
+                NewWeightInt += num;
+                switch (num)
+                {
+                    case 100:
+                        HundredWeight++;
+                        break;
+                    case 10:
+                        TenWeight++;
+                        break;
+                    case 1:
+                        OneWeight++;
+                        break;
+
+                }
+            }
+        }
+
+        void SubtractWeight(string i)
+        {
+            if (int.TryParse(i, out int num))
+            {
+                NewWeightInt -= num;
+                switch (num)
+                {
+                    case 100:
+                        HundredWeight--;
+                        break;
+                    case 10:
+                        TenWeight--;
+                        break;
+                    case 1:
+                        OneWeight--;
+                        break;
+
+                }
+            }
+        }
+
+        private int hundredWeight = 0;
+        public int HundredWeight
+        {
+            get => hundredWeight;
+            set
+            {
+                hundredWeight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int tenWeight = 0;
+        public int TenWeight
+        {
+            get => tenWeight;
+            set
+            {
+                tenWeight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int oneWeight = 0;
+        public int OneWeight
+        {
+            get => oneWeight;
+            set
+            {
+                oneWeight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void On_NextWorkoutInfo(object obj)
+        {
+            NextWorkoutInfo();
+        }
+
+        private bool canGoNext = true;
+        public bool CanGoNext
+        {
+            get => canGoNext;
+            set
+            {
+                canGoNext = value;
+                ((Command)NextCommand).ChangeCanExecute();
+            }
+        }
+
+        private void On_PreviousWorkoutInfo(object obj)
+        {
+            PreviousWorkoutInfo();
+        }
+
+        private bool canGoBack = false;
+        public bool CanGoBack
+        {
+            get => canGoBack;
+            set
+            {
+                canGoBack = value;
+                ((Command)PreviousCommand).ChangeCanExecute();
+            }
+        }
+
+        List<List<string>> DataArrayList = new List<List<string>>();
+        List<string> NewWeightList = new List<string>();
+        void RegisterNewWeight(bool AddOrDelete)
+        {
+
+            if (AddOrDelete)
+            {
+                NewWeightList.Add(NewWeightInt.ToString());
+            }
+            else
+            {
+                if((NewWeightList.Count - 1 ) >= 0)
+                {
+                    NewWeightList.RemoveAt(NewWeightList.Count - 1);
+                }
+            }
+        }
         void NextWorkoutInfo()
         {
-           
+            RegisterNewWeight(true);
+
             weightInt++;
             CanGoBack = true;
             if (weightInt == ((jsonObject.Sets.Count * workouts.Count)-1))
             {
-                NextBtnString = "Done";
+                
                 CanGoNext = false;
             }
 
@@ -139,17 +265,34 @@ namespace FitWatch.ViewModel
                 workAndRepInt++;
                 WorkoutTitletString = workouts[workAndRepInt];
                 RepString = reps[workAndRepInt];
+
+                // one array worth loaded up
+                DataArrayList.Add(NewWeightList);
+                NewWeightList = new List<string>();
                 
             }
 
-            RegisterNewWeight(true);
+            // start of new weight entry
+            NewWeightInt = 0;
+            OneWeight = 0;
+            TenWeight = 0;
+            HundredWeight = 0;
+            
         }
-
-        //todo: fix ordering of iteration
 
         void PreviousWorkoutInfo()
         {
 
+            // populate views
+            if (weightInt % 6 != 0)
+            {
+                ShowPrevious();
+            }
+                
+
+            RegisterNewWeight(false);
+
+            
             if (weightInt % 6 == 0)
             {
                 if (!(workAndRepInt <= 0))
@@ -159,6 +302,13 @@ namespace FitWatch.ViewModel
                 WorkoutTitletString = workouts[workAndRepInt];
                 RepString = reps[workAndRepInt];
 
+                // remove previous array item
+                // remove top array item
+                // make removed list the current
+                NewWeightList = DataArrayList[DataArrayList.Count - 1];
+                ShowPrevious();
+                DataArrayList.RemoveAt(DataArrayList.Count - 1);
+                NewWeightList.RemoveAt(NewWeightList.Count - 1);
             }
 
             weightInt--;
@@ -166,37 +316,44 @@ namespace FitWatch.ViewModel
 
             if (weightInt <= 0)
             {
-                NextBtnString = "Done";
+                
                 CanGoBack = false;
             }
 
             WeightString = weights[weightInt];
 
             
-
-            RegisterNewWeight(false);
+            
         }
 
-        List<string> NewWeightList = new List<string>();
-        void RegisterNewWeight(bool AddOrDelete)
+        void ShowPrevious()
         {
-            //todo: add option to go back, delete previous number
-            if (AddOrDelete)
-                NewWeightList.Add(NewWeightString);
-            else
-                NewWeightList.Remove(NewWeightString);
-        }
-
-        private string newWeightString;
-        public string NewWeightString
-        {
-            get => newWeightString;
-            set
+            string currentWeightInList = NewWeightList[NewWeightList.Count - 1];
+            NewWeightInt = Int32.Parse(currentWeightInList);
+            HundredWeight = (int)Math.Truncate((double)(newWeightInt / 100));
+            try
             {
-                newWeightString = value;
-                OnPropertyChanged();
+                TenWeight = Int32.Parse(currentWeightInList[currentWeightInList.Length - 2].ToString());
+                OneWeight = Int32.Parse(currentWeightInList[currentWeightInList.Length - 1].ToString());
             }
+            catch
+            {
+                try
+                {
+                    TenWeight = 0;
+                    OneWeight = Int32.Parse(currentWeightInList[currentWeightInList.Length - 1].ToString());
+                }
+                catch
+                {
+                    TenWeight = 0;
+                    OneWeight = 0;
+                }
+                
+            }
+            
         }
+
+        // binding label information -----------------------------
 
         private string workoutTitleString;
         public string WorkoutTitletString
@@ -231,15 +388,6 @@ namespace FitWatch.ViewModel
             }
         }
 
-        private string nextBtnString = "Next";
-        public string NextBtnString
-        {
-            get => nextBtnString;
-            set
-            {
-                nextBtnString = value;
-                OnPropertyChanged();
-            }
-        }
+        
     }
 }
